9c3018001a6172d96932ee968e2d2162f8374a46
Nandom Alfred <nandommamdam@gmail.com>
2023-10-26 22:32:51 +0100

    [200~let mut s = String::from("hello");  // push_str() appends a literal to a String

--- 03_ownership/src/main.rs
+++ 03_ownership/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    {                           // s is not valide here, it's not yet declared
+        let s = "hello";  // s is valid from thi poit forwards
+
+        println!("{s}")
+    }                           // this scope is now over, and is no longer valid
+
 }
