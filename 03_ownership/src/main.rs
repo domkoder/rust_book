0fcf84ce3cba0b2dc03763f8dc51f68b34617aa9
Nandom Alfred <nandommamdam@gmail.com>
2023-10-27 01:02:41 +0100

    Variable and Data Interacting with Move

--- 03_ownership/src/main.rs
+++ 03_ownership/src/main.rs
@@ -1,14 +1,21 @@
 fn main() {
+    // Varoable Scope
     {                           // s is not valide here, it's not yet declared
         let s = "hello";  // s is valid from thi poit forwards
-
         println!("{s}")
     }                           // this scope is now over, and is no longer valid
 
+    // The String Type
     let mut s = String::from("hello");  // push_str() appends a literal to a String
-    
     s.push_str(", world!");
-
     println!("{}", s);
 
+    // Variable and Data Interacting with Move
+    let x = 5;
+    let y = x;
+
+    let s1 = String::from("hello");
+    let s2 = s1;
+
+    println!("{}, world", s1);
 }
