e167a1f15994e7d25e316046bafbb7a359d30b4a
Nandom Alfred <nandommamdam@gmail.com>
2023-11-03 02:33:49 +0100

    [200~Mutable References

--- 04_borrowing/src/main.rs
+++ 04_borrowing/src/main.rs
@@ -1,11 +1,27 @@
 fn main() {
-    let s1: String = String::from("hello");
+    let mut s1: String = String::from("hello");
 
     let len: usize = calculate_length(&s1);
 
-    println!("The length of '{}' is {}", s1, len)
+    // Mutable References
+    change(&mut s1);
+
+    println!("The length of '{}' is {}", s1, len);
+
+    let mut s: String = String::from("Hello");
+    {
+        let r1: &mut String = &mut s; 
+    }; //r1 goes out of scope here, so we can make a new reference with no problems.
+    let r2: &mut String = &mut s;
+
+    println!("{}", r2);
 }
 
-fn calculate_length(s: &String) -> usize {
+fn calculate_length(s: &String) -> usize { // s is a reference to a String 
     s.len()
+} // Here, s goes out of scope. But because it doees not have ownership of what it refers to, it is not dropped.
+
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
