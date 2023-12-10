f16887672aa17963e7ebd061005c6114c8f15098
Nandom Alfred <nandommamdam@gmail.com>
2023-12-11 00:45:58 +0100

    Example program using structs

--- 05_structs/src/main.rs
+++ 05_structs/src/main.rs
@@ -1,36 +1,14 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64
-}
-// Using Tuple Structs Without Named Fields to Create Different Types
-struct Color(i32, i32, i32);
-struct Point(i32, i32,i32);
-
-
-
-fn build_user(username: String, email:String, )-> User{
-    User {
-        active: true,
-        username,
-        email,
-        sign_in_count: 1
-    }
-}
-
 fn main() {
-    let user1 = build_user(
-        String::from("testnet"), 
-        String::from("test@gmail.com") 
-    );
+    let width1 = 30;
+    let height1 = 50;
 
-    let user2 = User {
-        email: String::from("another@example.com"),
-        ..user1
-    };
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    )
 
-    let black = Color(0, 0, 0);
-    let origin = Point(0, 0, 0);
+}
 
+fn area(width:u32, height:u32) -> u32 {
+    width * height
 }
