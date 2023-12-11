8e9985f3b5f57e986baba1ac4f5eed5c4f09389d
Nandom Alfred <nandommamdam@gmail.com>
2023-12-11 03:55:53 +0100

    Method syntax/Defining methods

--- 05_structs/src/main.rs
+++ 05_structs/src/main.rs
@@ -5,17 +5,30 @@ struct  Rectangel {
     height: u32,
 }
 
+impl Rectangel {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+}
+
 fn main() {
-    let scale: u32 = 2;
     let rect1: Rectangel = Rectangel {
-        width: dbg!(30 * scale),
+        width: 30,
         height: 50,
     };
     dbg!(&rect1);
-    println!("rect1 is {:#?}", rect1);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+    
+    if rect1.width() {
+    println!("The rectangle has a nonzero width; it is {}", rect1.width)
+    };
 
 }
 
-fn area(rectangel: &Rectangel) -> u32 {
-    rectangel.width * rectangel.height
-}
