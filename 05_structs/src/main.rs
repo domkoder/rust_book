b425acb2ddac01c8d97ceb2861b93d3c7509e360
Nandom Alfred <nandommamdam@gmail.com>
2023-12-11 04:09:33 +0100

    Multiple impl Blocks

--- 05_structs/src/main.rs
+++ 05_structs/src/main.rs
@@ -13,6 +13,10 @@ impl Rectangel {
     fn width(&self) -> bool {
         self.width > 0
     }
+
+    fn can_hold(&self, other:&Rectangel)-> bool{
+        self.width > other.width && self.height > other.height 
+    }
 }
 
 fn main() {
@@ -20,15 +24,31 @@ fn main() {
         width: 30,
         height: 50,
     };
+
+    let rect2: Rectangel = Rectangel {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3: Rectangel = Rectangel {
+        width: 60,
+        height: 45
+    };
+
+
     dbg!(&rect1);
+
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
-    
+
     if rect1.width() {
-    println!("The rectangle has a nonzero width; it is {}", rect1.width)
+        println!("The rectangle has a nonzero width; it is {}", rect1.width)
     };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect2.can_hold(&rect3));
 
 }
 
