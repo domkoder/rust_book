71939c2d8bc20de3a9856e7fb70e595ce9773215
Nandom Alfred <nandommamdam@gmail.com>
2023-12-12 05:43:34 +0100

    Reads and Writes with &self and &mut self

--- 05_structs/src/main.rs
+++ 05_structs/src/main.rs
@@ -14,60 +14,81 @@ impl Rectangel {
         self.width = width;
     }
 
-    fn square(size: u32) -> Self {
-        Self {
-            width: size,
-            height: size
+    fn max(self, other: Rectangel)-> Rectangel {
+        Rectangel {
+            width: self.width.max(other.width),
+            height: self.height.max(other.height),
         }
     }
 }
 
-impl Rectangel {
-    fn can_hold(&self, other:&Rectangel)-> bool{
-        self.width > other.width && self.height > other.height 
-    }
-}
+// impl Rectangel {
+//     fn can_hold(&self, other:&Rectangel)-> bool{
+//         self.width > other.width && self.height > other.height 
+//     }
+
+//     fn square(size: u32) -> Self {
+//         Self {
+//             width: size,
+//             height: size
+//         }
+//     }
+// }
 
 fn main() {
-    let rect1: Rectangel = Rectangel {
-        width: 30,
-        height: 50,
+    // let rect1: Rectangel = Rectangel {
+    //     width: 30,
+    //     height: 50,
+    // };
+
+    // let rect2: Rectangel = Rectangel {
+    //     width: 10,
+    //     height: 40,
+    // };
+
+    // let rect3: Rectangel = Rectangel {
+    //     width: 60,
+    //     height: 45
+    // };
+
+    // let mut r: Rectangel = Rectangel{
+    //     width: 1,
+    //     height: 2
+    // };
+
+    // let area1: u32 = r.area();
+    // let area2: u32 = Rectangel::area(&r);
+    // assert_eq!(area1, area2);
+
+    // r.set_width(2);
+    // Rectangel::set_width(&mut r, 2);
+
+    // Rectangel::square(20);
+
+
+    // dbg!(&rect1);
+
+    // println!(
+    //     "The area of the rectangle is {} square pixels.",
+    //     rect1.area()
+    // );
+
+    // println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    // println!("Can rect1 hold rect3? {}", rect2.can_hold(&rect3));
+
+    let mut rect: Rectangel = Rectangel {
+        width: 0,
+        height:0
     };
 
-    let rect2: Rectangel = Rectangel {
-        width: 10,
-        height: 40,
-    };
+    rect.set_width(1);
 
-    let rect3: Rectangel = Rectangel {
-        width: 60,
-        height: 45
-    };
+    let rect_ref: &Rectangel = &rect;
 
-    let mut r: Rectangel = Rectangel{
-        width: 1,
-        height: 2
-    };
+    rect_ref.set_width(2);
 
-    let area1: u32 = r.area();
-    let area2: u32 = Rectangel::area(&r);
-    assert_eq!(area1, area2);
+    println!("{}", rect.area());
 
-    r.set_width(2);
-    Rectangel::set_width(&mut r, 2);
-
-    Rectangel::square(20);
-
-
-    dbg!(&rect1);
-
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.area()
-    );
-
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect2.can_hold(&rect3));
 
 }
 
