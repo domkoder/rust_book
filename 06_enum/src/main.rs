4795a1902c2abf7c181e002b2c0595d9dc81e6c3
Nandom Alfred <nandommamdam@gmail.com>
2023-12-15 02:35:02 +0100

    Defining an Enum

--- 06_enum/src/main.rs
+++ 06_enum/src/main.rs
@@ -1,8 +1,29 @@
-
-enum IpAddrKind {
-    v4,
-    v6,
+#[derive(Debug)]
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
+#[derive(Debug)]
+enum Message {
+    Quite,
+    Move {x: i32, y:i32},
+    Write(String),
+    ChangeColor(i32, i32, i32)
+}
+
+impl Message {
+    fn call (&self) {
+        println!("{:?}", self);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let home: IpAddr = IpAddr::V4(127, 0, 0, 1);
+
+    let loopback: IpAddr = IpAddr::V6(String::from("::1"));
+
+    println!("{:#?} {:#?}", home, loopback);
+
+    let m = Message::Write(String::from("Hellow"));
+    m.call()
 }
