3d5f999ebfff83a45ae02641ab03c358c0a05015
Nandom Alfred <nandommamdam@gmail.com>
2023-12-23 03:30:38 +0100

    Patterns that bind to values

--- 06_enum/src/main.rs
+++ 06_enum/src/main.rs
@@ -1,8 +1,14 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -13,11 +19,14 @@ fn value_in_cents(coin: Coin) -> u8 {
         },
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
     }
 }
 
 
 fn main() {
-    value_in_cents(Coin::Penny);
+    value_in_cents(Coin::Quarter(UsState::Alaska));
 }
